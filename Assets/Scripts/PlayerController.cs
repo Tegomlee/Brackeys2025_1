77480d54befebb3e5907d790acd46c2e40a48a11
Jerry19Kx <168358817+Jerry19Kx@users.noreply.github.com>
2025-02-20 21:01:27 +0100

    entrance of bank
    some sprites put together, want to add a wet floor in the first room and maybe a worker that asks character why he is wearing a mask or jeb (thats how i named hackerfriend)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 2f;
+    private Vector3 movementDirection;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private bool isGrounded = false;
+
+    private SpriteRenderer spriteRenderer;
+  
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        ProcessPlayerInputs();
+    }
+
+    private void FixedUpdate() {
+        if(isGrounded){
+            MovePlayer();
+            // Change player animation when moving
+            animator.SetFloat("xVelocity", Math.Abs(rb.linearVelocity.x));
+        }
+        
+    }
+
+    // Decide the movement direction based on player inputs
+    private void ProcessPlayerInputs(){
+        
+        float horizontalMovement;
+        // float verticalMovement;
+        
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        // verticalMovement = Input.GetAxisRaw("Vertical");
+     
+        movementDirection = new Vector3(horizontalMovement, 0).normalized;
+    }
+
+    // Apply the movement in direction & with playerSpeed
+    private void MovePlayer() {
+        FlipPlayer(movementDirection.x);
+        rb.linearVelocity = movementDirection*playerSpeed;
+    }
+
+    private void FlipPlayer(float xDirection){
+        if(xDirection < 0){
+            spriteRenderer.flipX = false;
+        }else if(xDirection > 0){
+            spriteRenderer.flipX = true;
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Exclude the player layer to avoid self collision trigger
+        if(collision.gameObject.layer != 3)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != 3)
+        {
+            isGrounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        // Exclude the player layer to avoid self collision trigger
+        if(collision.gameObject.layer != 3){
+            isGrounded = false;
+        }
+    }
+}
